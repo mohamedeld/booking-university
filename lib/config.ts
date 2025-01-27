cfa01c7cd9100a8f0bfdff005357f59b82e8018f
mohamedeld <mohamed.azoz20010@gmail.com>
2025-01-27 22:33:08 +0200

    add drizzle with neon

--- lib/config.ts
+++ lib/config.ts
@@ -5,7 +5,8 @@
       publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       urlEndpoint:process.env.NEXT_PUBLIC_IMAGEKIT_ENDPOINT!,
       privateKey:process.env.IMAGEKIT_URL_PRIVATE_KEY!
-    }
+    },
+    databaseUrl:process.env.DATABASE_URL!
   }
 }
 
